7285cba5cdd92ccaac1430c39886eaa9f369fadd
Wiphoo Methachawalit <wiphoo.methachawalit@refinitiv.com>
2019-10-15 18:27:18 +0700

    added setup.py for distribute package

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup( name = 'simple_percepton',  
+                    version = '0.1',
+                    author = 'Wiphoo Methachawalit',\
+                    description = 'A simple percepton package for understanding neural network concpet',
+                    url="https://github.com/wiphoo/neural-simple_perceptron",
+                    packages=setuptools.find_packages(),
+                    classifiers=[
+                        "Programming Language :: Python :: 3",
+                        "License :: OSI Approved :: MIT License",
+                        "Operating System :: OS Independent",
+                    ],
+ )
