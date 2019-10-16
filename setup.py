2e58adfa6c77d0d3060cc4c8685a45b20094e745
Wiphoo Methachawalit <wiphoo.methachawalit@refinitiv.com>
2019-10-16 10:44:50 +0700

    added missing required numpy package

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 import setuptools
 
 setuptools.setup( name = 'simple_percepton',  
-                    version = '0.1',
+                    version = '0.1.1',
                     author = 'Wiphoo Methachawalit',\
-                    description = 'A simple percepton package for understanding neural network concpet',
-                    url="https://github.com/wiphoo/neural-simple_perceptron",
-                    packages=setuptools.find_packages(),
-                    classifiers=[
-                        "Programming Language :: Python :: 3",
-                        "License :: OSI Approved :: MIT License",
-                        "Operating System :: OS Independent",
+                    description = 'A simple percepton package for understanding neural network concept',
+                    url = "https://github.com/wiphoo/neural-simple_perceptron",
+                    packages = setuptools.find_packages(),
+                    install_requires = [ 
+                                        'numpy',
+                    ],
+                    classifiers = [
+                            "Programming Language :: Python :: 3",
+                            "License :: OSI Approved :: MIT License",
+                            "Operating System :: OS Independent",
                     ],
  )
